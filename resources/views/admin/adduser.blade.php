03e16352c30c3415af0fc6d0ff923f27840a439c
Rashid Julasiri <rashidjulasiri@gmail.com>
2021-05-17 16:02:44 +0800

    Crud Users from Admin Side

---
+++ resources/views/admin/adduser.blade.php
@@ -0,0 +1,104 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Create New User</div>
+
+                <div class="card-body">
+                    <form  action="/admin/listuser" method="POST">
+                        @csrf
+                        <div class="form-group row">
+                            <label for="name" class="col-md-4 col-form-label text-md-right">First Name</label>
+
+                            <div class="col-md-6">
+                                <input id="fname" type="text" class="form-control @error('fname') is-invalid @enderror" name="fname" value="{{ old('fname') }}" required autocomplete="fname" autofocus>
+
+                                @error('fname')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="name" class="col-md-4 col-form-label text-md-right">Last Name</label>
+
+                            <div class="col-md-6">
+                                <input id="lname" type="text" class="form-control @error('lname') is-invalid @enderror" name="lname" value="{{ old('lname') }}" required autocomplete="lname" autofocus>
+
+                                @error('lname')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <div class="form-check mx-auto">
+                                <label class="form-check-label">
+                                  <input type="radio" class="form-check-input" name="role" id="admin" value="1">
+                                  Admin
+                                </label>
+                                <label class="form-check-label ml-5">
+                                  <input type="radio" class="form-check-input" name="role" id="user" value="0">
+                                  User
+                                </label>
+                              </div>
+                        </div>
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-6 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                   Submit
+                                </button>
+                                <a href="{{ url('/admin/listuser')}}" class="btn btn-secondary my-3" role="button"> Cancel</a>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
--- resources/views/admin/listuser.blade.php
+++ resources/views/admin/listuser.blade.php
@@ -4,57 +4,51 @@
 <div class="container">
     <div class="row">
 
-        <div class="col-md-12">
-            <h1>Admin Dashboard | Category</h1>
-            @if (Session::has('success'))
-                <div class="col-md-12 alert alert-success mt-5 text-center text-uppercase my-3" role="alert" >
-                    <strong><p>{{ Session::get('success')}} </p></strong>
-                </div>
-            @endif
-            <ul class="list-inline border-bottom my-3 py-2">
+        <div class="col-md-12 mb-2">
+            <h1>Admin Dashboard | List of Users</h1>
+
+            <ul class="list-inline border-bottom py-2">
                 <li class="list-inline-item"><a href="{{url('/admin')}}" class="text-decoration-none">Catergory</a></li>
                 <li class="list-inline-item ml-4"><a href="{{url('/admin/listuser')}}" class="text-decoration-none">Users</a></li>
               </ul>
-            <div class="card text-white bg-info mb-3 mx-auto" style="max-width:25rem;">
-                <div class="card-body">
-                  <h5 class="card-title">Title: {{$category->title}}</h5>
-                  <p class="card-text">Description: {{$category->description}}</p>
-            </div>
         </div>
 
+        @if (Session::has('success'))
+            <div class="col-md-12 alert alert-success mt-5 text-center text-uppercase" role="alert" >
+                <strong><p>{{ Session::get('success')}} </p></strong>
+            </div>
+        @endif
+
         <div class="col-md-12">
-            <a href="{{url('/admin/createcategory')}}" class="btn btn-success my-2 mr-auto" role="button">
+            <a href="{{url('/admin/adduser')}}" class="btn btn-success my-2 mr-auto float-right" role="button">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
                     <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
                     <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
-                </svg> Add Questionnaire
+                </svg> Add User
             </a>
 
             <table class="table">
                 <thead class="thead-dark">
                 <tr>
                     <th scope="col">ID</th>
-                    <th scope="col">Question</th>
-                    <th scope="col" colspan="4">Answers</th>
+                    <th scope="col">Full Name</th>
+                    <th scope="col">Email</th>
                     <th scope="col">Actions</th>
                 </tr>
                 </thead>
                 <tbody>
-
+                    @foreach ($users as $user)
                         <tr>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
+                            <td>{{$user->id}}</td>
+                            <td>{{$user->fname}} {{$user->lname}}</td>
+                            <td>{{$user->email}}</td>
                             <td>
                                 <ul class="list-inline">
                                     <li class="list-inline-item">
-                                        <a href="" class="btn btn-warning"  role="button"><i class="bi bi-pencil-square"></i> </a>
+                                        <a href="{{url('/admin/'.$user->id.'/edituser')}}" class="btn btn-warning"  role="button"><i class="bi bi-pencil-square"></i> </a>
                                     </li>
                                     <li class="list-inline-item">
-                                        <form action="" method="post">
+                                        <form action="/admin/listuser/{{$user->id}}" method="post">
                                             @csrf
                                             @method('DELETE')
                                             <button  class ="btn btn-danger">
@@ -65,6 +59,7 @@
                                 </ul>
                             </td>
                         </tr>
+                     @endforeach
                 </tbody>
             </table>
         </div>
